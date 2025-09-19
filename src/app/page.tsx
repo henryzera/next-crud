c75b641bc506fa684d50cc28a304070fc29482c7
Emanuel Henry <emanuelhenrysc@gmail.com>
2025-09-19 02:07:22 -0300

    General functioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,44 @@
+"use client"
+
 import Image from "next/image";
 import Layout from "@/components/Layout";
+import Tabela from "@/components/Tabela";
+import Cliente from "@/core/Cliente";
+import Botao from "@/components/Botao";
+import Formulario from "@/components/Formulario";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazrio())
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '4')
+  ]
+
+const clienteSelecionado = (cliente: Cliente) =>{
+  setCliente(cliente)
+  setVisivel('form')
+}
+
+const clienteExcluido = (cliente: Cliente) =>{
+  console.log(`Excluir... ${cliente.nome}`)
+}
+
+function salvarCliente(cliente: Cliente){
+  console.log(cliente)
+  setTimeout(()=>{setVisivel('tabela')}, 200)
+}
+
+function novoCliente(){
+  setCliente(Cliente.vazrio())
+  setVisivel('form')
+}
+
   return (
     <div className='
       flex justify-center items-center h-screen
@@ -9,7 +46,25 @@ export default function Home() {
       text-white
     '>
       <Layout titulo="Cadastro Simples">
-        Conteudo
+        { visivel === 'tabela' ? (
+          <>
+            <div className="flex justify-end">
+              <Botao 
+                onClick={novoCliente}
+                className="mb-4" 
+                cor="green">
+                  Novo Cliente
+              </Botao>
+            </div>
+            <Tabela clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido} clientes={clientes} ></Tabela>
+          </>
+        ) :
+          <Formulario
+            cliente={cliente}
+            clienteMudou={salvarCliente}
+            cancelado={() => setVisivel('tabela')}
+          />
+        }
       </Layout>
     </div>
   );
---
+++ src/components/Tabela.tsx
@@ -0,0 +1,78 @@
+import Cliente from "@/core/Cliente"
+import { EditIcon, DeleteIcon } from "./Icones"
+
+interface TabelaProps{
+    clientes: Cliente[]
+    clienteSelecionado?: (cliente: Cliente) => void
+    clienteExcluido?: (cliente: Cliente) => void
+}
+
+export default function Tabela(props: TabelaProps){
+
+    const actions = props.clienteExcluido || props.clienteSelecionado
+
+    function headRender(){
+        return(
+            <tr>
+                <th className="text-left p-4">Código</th>
+                <th className="text-left p-4">Nome</th>
+                <th className="text-left p-4">Idade</th>
+                { actions ? <th className="text-center">Ações</th> : false}
+            </tr>
+        )
+    }
+
+    function clientsRender(clients: Cliente[]){
+        return clients?.map((client:Cliente, i:number)=>{
+            return(
+                <tr className={i % 2 == 0 ? 'bg-purple-200' : 'bg-purple-100'} key={client.id}>
+                    <td className="text-left p-4">{client.id}</td>
+                    <td className="text-left p-4">{client.nome}</td>
+                    <td className="text-left p-4">{client.idade}</td>
+                    {actionsRender(client)}
+                </tr>
+            )
+        })
+    }
+
+    function actionsRender(client: Cliente){
+        return(
+            <td className="flex justify-center">
+                {props.clienteSelecionado ? (
+                    <button onClick={() => props.clienteSelecionado?.(client)} className="
+                        flex justify-center items-center
+                        text-green-600 rounded-full p-2 m-1
+                        hover:bg-purple-50
+                    ">
+                        {EditIcon}
+                    </button>
+                ) : false}
+
+                {props.clienteExcluido ? (
+                    <button onClick={() => props.clienteExcluido?.(client)} className="
+                        flex justify-center items-center
+                        text-red-500 rounded-full p-2 m-1
+                        hover:bg-purple-50
+                    ">
+                        {DeleteIcon}
+                    </button>
+                ) : false}
+                
+            </td>
+        )
+    }
+
+    return(
+        <table className="w-full rounded-xl overflow-hidden">
+            <thead className="
+                text-gray-100
+                bg-gradient-to-r from-purple-700 to-purple-900
+            ">
+                {headRender()}
+            </thead>
+            <tbody className="">
+                {clientsRender(props.clientes)}
+            </tbody>
+        </table>
+    )
+}
